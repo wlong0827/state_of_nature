acb8d7c92e2da3c3138b388b2f61add20922faf3
William Long <wlong0827@gmail.com>
2018-10-31 23:58:52 -0400

    added image

--- state_of_nature_game.py
+++ state_of_nature_game.py
@@ -12,67 +12,67 @@ plotly.tools.set_credentials_file(username='wlong0827', api_key='V74dVKOUU8tUsPv
 
 def main():
 
-	scores = []
+    scores = []
 
-	for trial in range(trials):
-	    game = StateOfNature(board_size)
-	    player_1 = QLPlayer("P1")
-	    player_2 = QLPlayer("P2")
-	    players = [player_1, player_2]
-	    player_ids = ["P1", "P2"]
+    for trial in range(trials):
+        game = StateOfNature(board_size)
+        player_1 = QLPlayer("P1")
+        player_2 = QLPlayer("P2")
+        players = [player_1, player_2]
+        player_ids = ["P1", "P2"]
 
-	    rewards = defaultdict(list)
+        rewards = defaultdict(list)
 
-	    for step in range(n_steps):
-	        state = game.get_cur_state()
-	        turn = game.get_cur_turn()
-	        player = players[turn]
+        for step in range(n_steps):
+            state = game.get_cur_state()
+            turn = game.get_cur_turn()
+            player = players[turn]
 
-	        a = player.act(state, player_ids, board_size)
+            a = player.act(state, player_ids, board_size)
 
-	        r, state_next = game.move(a)
+            r, state_next = game.move(a)
 
-	        player.update_Q(state, r, a, state_next)
+            player.update_Q(state, r, a, state_next)
 
-	        rewards[player].append(r)
+            rewards[player].append(r)
 
-	    p1_score = sum(list(rewards[player_1]))
-	    p2_score = sum(list(rewards[player_2]))
+        p1_score = sum(list(rewards[player_1]))
+        p2_score = sum(list(rewards[player_2]))
 
-	    print "Trial {} results".format(trial)
-	    print "----------------------------"
-	    print "Player 1 total score: ", p1_score
-	    print "Player 2 total score: ", p2_score
-	    print "\n"
-	    scores.append((p1_score, p2_score))
+        print "Trial {} results".format(trial)
+        print "----------------------------"
+        print "Player 1 total score: ", p1_score
+        print "Player 2 total score: ", p2_score
+        print "\n"
+        scores.append((p1_score, p2_score))
 
-	trace1 = {
-			"y": [score[0] for score in scores], 
-          	"x": ["Trial " + str(trial) for trial in range(trials)], 
-          	"marker": {"color": "pink", "size": 12}, 
-          	"mode": "markers", 
-          	"name": "Q-Learning Player", 
-          	"type": "scatter"
-			}		
+    trace1 = {
+                "y": [score[0] for score in scores], 
+                "x": ["Trial " + str(trial) for trial in range(trials)], 
+                "marker": {"color": "pink", "size": 12}, 
+                "mode": "markers", 
+                "name": "Q-Learning Player", 
+                "type": "scatter"
+            }       
 
-	trace2 = {
-			"y": [score[1] for score in scores], 
-          	"x": ["Trial " + str(trial) for trial in range(trials)], 
-          	"marker": {"color": "blue", "size": 12}, 
-          	"mode": "markers", 
-          	"name": "Q-Learning Player", 
-          	"type": "scatter"
-			}	
+    trace2 = {
+                "y": [score[1] for score in scores], 
+                "x": ["Trial " + str(trial) for trial in range(trials)], 
+                "marker": {"color": "blue", "size": 12}, 
+                "mode": "markers", 
+                "name": "Q-Learning Player", 
+                "type": "scatter"
+            }   
 
-	data = [trace1, trace2]
-	layout = {"title": "Q-Learning vs Q-Learning Player", 
-	          "xaxis": {"title": "Trial", }, 
-	          "yaxis": {"title": "Total Score over 10000 Steps"}}
+    data = [trace1, trace2]
+    layout = {"title": "Q-Learning vs Q-Learning Player", 
+              "xaxis": {"title": "Trial", }, 
+              "yaxis": {"title": "Total Score over 10000 Steps"}}
 
-	print "Producing plots..."
-	fig = go.Figure(data=data, layout=layout)
-	py.iplot(fig, filename='QLearn vs QLearn')
-	print "Finished"
+    print "Producing plots..."
+    fig = go.Figure(data=data, layout=layout)
+    py.iplot(fig, filename='QLearn vs QLearn')
+    print "Finished"
 
 if __name__ == "__main__":
     main()
